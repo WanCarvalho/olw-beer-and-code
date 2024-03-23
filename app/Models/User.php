0b3ab2efb8fb29ce3800b5a07f0298fe9459ec55
WanCarvalho <wandersoncarvalho.1501@gmail.com>
2024-03-23 14:54:04 -0300

    update: relacionamentos entre company, clients, role. E migration para chaves FK

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -49,5 +51,9 @@ class User extends Authenticatable
     public function seller(): HasOne
     {
         return $this->hasOne(Seller::class);
+    }
+
+    public function role(): BelongsTo {
+        return $this->belongsTo(Role::class);
     }
 }
